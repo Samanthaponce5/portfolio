e4848aa2fb99fbb534cd252f25101e92e8eae6da
Samanthaponce5 <samanthaponce5@gmail.com>
2020-10-29 14:07:49 -0400

    shopify

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,6 +31,28 @@ export default class Projects extends React.Component{
             <div class="carousel-item item-2">
               <h2>Project 2</h2>
               <div className='projectInfo'>
+              <h1 >ShopMart</h1>
+              <p className='describe'>An e-commerce site where you can shop around for many options of clothing.
+               </p>
+               <ul className='stack'>
+                 <li>ReactJs</li>
+                 <li>Redux</li>
+                 <li>Ruby on Rails backend</li>
+                 <li>Postgres Databse</li>
+               </ul>
+               <button className='viewbtn' ><a href='https://github.com/Samanthaponce5/shopMart-front'>View Code</a></button>
+               <button className='viewbtn' ><a href='https://shop-mart-us.netlify.app/'>Site</a></button>
+
+              </div>
+              <a class="arrow arrow-prev" href="#item-1"></a>
+              <a class="arrow arrow-next" href="#item-3"></a>
+            </div>
+
+
+
+            <div class="carousel-item item-3">
+              <h2>Project 3</h2>
+              <div className='projectInfo'>
               <h1 >Picology</h1>
               <p className='describe'>An e-commerce site where you can shop around for many options of photography.
                </p>
@@ -41,15 +63,15 @@ export default class Projects extends React.Component{
                  <li>Postgres Databse</li>
                </ul>
                <button className='viewbtn' ><a href='https://github.com/Samanthaponce5/Ecomm-frontend'>View Code</a></button>
-               <button className='viewbtn' ><a href='https://youtu.be/SsByJf9NJJ4'>Demo</a></button>
+               <button className='viewbtn' ><a href='https://www.youtube.com/watch?v=SsByJf9NJJ4&t=3s'>Demo</a></button>
 
               </div>
-              <a class="arrow arrow-prev" href="#item-1"></a>
+              <a class="arrow arrow-prev"  href="#item-2"></a>
               {/* <a class="arrow arrow-next" href="#item-3"></a> */}
             </div>
 {/*             
             <div class="carousel-item item-3">
-              <h2>Item 3</h2>
+              <h2>Project 3</h2>
               <div className='projectInfo'></div>
               <a class="arrow arrow-prev" href="#item-2"></a>
               <a class="arrow arrow-next" href="#item-1"></a>
